8d705ee903eb1bd2f62a36e3e540061a4dbf9021
admin <hain3628@gmail.com>
2023-07-18 00:10:17 +0700

    18/7(00:10)

--- app/src/main/java/com/example/alarmoclock/CustomAdapter.kt
+++ app/src/main/java/com/example/alarmoclock/CustomAdapter.kt
@@ -2,26 +2,61 @@ package com.example.alarmoclock
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.ToggleButton
+import java.util.Calendar
 
 class CustomAdapter(val activity: Activity, val list: MutableList<DataClock>) :ArrayAdapter<DataClock>(activity,R.layout.layout_custom_time) {
     override fun getCount(): Int {
         return list.size
     }
-
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var contexts = activity.layoutInflater
         var rowView = contexts.inflate(R.layout.layout_custom_time,parent,false)
         var tvTime = rowView.findViewById<TextView>(R.id.tvTime)
         var tvNameAlarm = rowView.findViewById<TextView>(R.id.tvNameAlarm)
+        var tbOnOff = rowView.findViewById<ToggleButton>(R.id.tbONOFF)
 
         tvTime.setText(list[position].time)
         tvNameAlarm.setText(list[position].nameAlarm)
+        var intent = Intent(activity,AlarmReceiver::class.java)
+
+        tbOnOff.setOnCheckedChangeListener {_, isChecked ->
+            var time:Long
+            if(isChecked){
+                Toast.makeText(activity,"Alarm On", Toast.LENGTH_SHORT).show()
+                pendingIntent = PendingIntent.getBroadcast(activity,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+                time = today.timeInMillis - today.timeInMillis % 60000
+                if(System.currentTimeMillis() > time ){
+                    time = if (Calendar.AM_PM == 0 ){
+                        time + 1000*60*60*12
+                    }
+                    else{
+                        time + 1000*60*60*24
+                    }
+                }
+                alarmManager!!.setRepeating(AlarmManager.RTC_WAKEUP,time,1000, pendingIntent!!)
+
+            }
+            else {
+                alarmManager!!.cancel(pendingIntent!!)
+                Toast.makeText(activity,"Alarm OFF", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
         return rowView
     }
 
--- app/src/main/java/com/example/alarmoclock/MainActivity.kt
+++ app/src/main/java/com/example/alarmoclock/MainActivity.kt
@@ -22,8 +22,9 @@ private lateinit var binding: ActivityMainBinding
 private var list = mutableListOf<DataClock>()
 val today = Calendar.getInstance()
 private lateinit var customAdapter: CustomAdapter
-private var pendingIntent: PendingIntent? = null
-private var alarmManager:AlarmManager? = null
+var pendingIntent: PendingIntent? = null
+var alarmManager: AlarmManager? = null
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,56 +37,16 @@ class MainActivity : AppCompatActivity() {
         }
         getValue()
         showItemAlarm()
-        var tbOnOff = customAdapter.activity.findViewById<ToggleButton>(R.id.tbONOFF)
-        tbOnOff.setOnCheckedChangeListener {_, isChecked ->
-            var time:Long
-            if(isChecked){
-                Toast.makeText(this,"Alarm On",Toast.LENGTH_SHORT).show()
-
-                pendingIntent = PendingIntent.getBroadcast(
-                    this,
-                    0,
-                    intent,
-                    PendingIntent.FLAG_IMMUTABLE
-                )
-                time = today.timeInMillis - today.timeInMillis % 60000
-                if(System.currentTimeMillis() > time ){
-                    time = if (Calendar.AM_PM == 0 ){
-                        time + 1000*60*60*12
-                    }
-                    else{
-                        time + 1000*60*60*24
-                    }
-                }
-                alarmManager!!.setRepeating(AlarmManager.RTC_WAKEUP,time,1000, pendingIntent!!)
-            }
-            else {
-                alarmManager!!.cancel(pendingIntent!!)
-                Toast.makeText(this,"Alarm OFF",Toast.LENGTH_SHORT).show()
-
-            }
-        }
-
     }
-
-    private fun deleteAlarm(){
-        binding.lvAlarm.setOnItemClickListener { parent, view, position, id ->
-            list.removeAt(position)
-        }
-    }
-
     private fun getValue() {
         var i = intent
         var itemListTime = i.getStringExtra("selectedTime")
         var itemListName = i.getStringExtra("giveName")
-        list.add(DataClock("$itemListTime","$itemListName"))
+        list.add(DataClock("$itemListTime", "$itemListName"))
     }
 
     private fun showItemAlarm() {
         customAdapter = CustomAdapter(this, list)
         binding.lvAlarm.adapter = customAdapter
     }
-
-
-
 }
