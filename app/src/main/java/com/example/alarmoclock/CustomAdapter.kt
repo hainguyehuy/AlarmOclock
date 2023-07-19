47ff61f47ba701cfd828a69d6792d83aeb9a35e0
admin <hain3628@gmail.com>
2023-07-20 01:12:31 +0700

    20/7

--- app/src/main/java/com/example/alarmoclock/CustomAdapter.kt
+++ app/src/main/java/com/example/alarmoclock/CustomAdapter.kt
@@ -4,61 +4,91 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlarmManager
 import android.app.PendingIntent
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.ToggleButton
-import java.util.Calendar
+import androidx.appcompat.app.AlertDialog
 
-class CustomAdapter(val activity: Activity, val list: MutableList<DataClock>) :ArrayAdapter<DataClock>(activity,R.layout.layout_custom_time) {
+class CustomAdapter(private val activity: Activity, private val list: MutableList<DataClock>) :
+    ArrayAdapter<DataClock>(activity, R.layout.layout_custom_time) {
     override fun getCount(): Int {
         return list.size
     }
+    @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var contexts = activity.layoutInflater
-        var rowView = contexts.inflate(R.layout.layout_custom_time,parent,false)
-        var tvTime = rowView.findViewById<TextView>(R.id.tvTime)
-        var tvNameAlarm = rowView.findViewById<TextView>(R.id.tvNameAlarm)
-        var tbOnOff = rowView.findViewById<ToggleButton>(R.id.tbONOFF)
+        val contexts = activity.layoutInflater
+        val rowView = contexts.inflate(R.layout.layout_custom_time, parent, false)
+        val tvTime = rowView.findViewById<TextView>(R.id.tvTime)
+        val tvNameAlarm = rowView.findViewById<TextView>(R.id.tvNameAlarm)
+        val tbOnOff = rowView.findViewById<ToggleButton>(R.id.tbONOFF)
 
-        tvTime.setText(list[position].time)
-        tvNameAlarm.setText(list[position].nameAlarm)
-        var intent = Intent(activity,AlarmReceiver::class.java)
+        tvTime.text = list[position].time
+        tvNameAlarm.text = list[position].nameAlarm
+        val intent = Intent(activity, AlarmReceiver::class.java)
 
-        tbOnOff.setOnCheckedChangeListener {_, isChecked ->
-            var time:Long
-            if(isChecked){
-                Toast.makeText(activity,"Alarm On", Toast.LENGTH_SHORT).show()
-                pendingIntent = PendingIntent.getBroadcast(activity,
-                    0,
-                    intent,
-                    PendingIntent.FLAG_IMMUTABLE
-                )
-                time = today.timeInMillis - today.timeInMillis % 60000
-                if(System.currentTimeMillis() > time ){
-                    time = if (Calendar.AM_PM == 0 ){
-                        time + 1000*60*60*12
+        tbOnOff.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                val dialog = AlertDialog.Builder(activity)
+                dialog.apply {
+                    //title
+                    setTitle("Confirm choose")
+                    //message
+                    setMessage("Do you want to  set an alarm?")
+                    // negative : phu dinh
+                    setNegativeButton("No") { dialogInterface: DialogInterface, i: Int ->
+                        dialogInterface.dismiss()
                     }
-                    else{
-                        time + 1000*60*60*24
+                    setPositiveButton("Yes") { dialogInterface: DialogInterface, i: Int ->
+                        Toast.makeText(activity,"Alarm on",Toast.LENGTH_SHORT).show()
+                        alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                        pendingIntent = PendingIntent.getBroadcast(
+                            context,
+                            111,
+                            intent,
+                            PendingIntent.FLAG_MUTABLE
+                        )
+                        alarmManager!!.setRepeating(AlarmManager.RTC_WAKEUP,
+                            today.timeInMillis,AlarmManager.INTERVAL_DAY, pendingIntent!!)
+
+
                     }
                 }
-                alarmManager!!.setRepeating(AlarmManager.RTC_WAKEUP,time,1000, pendingIntent!!)
-
-            }
-            else {
-                alarmManager!!.cancel(pendingIntent!!)
-                Toast.makeText(activity,"Alarm OFF", Toast.LENGTH_SHORT).show()
-
+                dialog.show()
             }
         }
-
         return rowView
     }
+//            var time:Long
+//            if(isChecked){
+//                Toast.makeText(activity,"Alarm On", Toast.LENGTH_SHORT).show()
+//                pendingIntent = PendingIntent.getBroadcast(activity,
+//                    0,
+//                    intent,
+//                    PendingIntent.FLAG_IMMUTABLE
+//                )
+//                time = today.timeInMillis - today.timeInMillis % 60000
+//                if(System.currentTimeMillis() > time ){
+//                    time = if (Calendar.AM_PM == 0 ){
+//                        time + 1000*60*60*12
+//                    }
+//                    else{
+//                        time + 1000*60*60*24
+//                    }
+//                }
+//                alarmManager!!.setRepeating(AlarmManager.RTC_WAKEUP,time,1000, pendingIntent!!)
+//
+//            }
+//            else {
+//                alarmManager!!.cancel(pendingIntent!!)
+//                Toast.makeText(activity,"Alarm OFF", Toast.LENGTH_SHORT).show()
+//            }
+//    }
 
 
 }
